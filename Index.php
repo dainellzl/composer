3be13528936cbe5a1ba9d2aa9d0011842ef06bc2
daniel <dainel_lzl@163.com>
2020-11-13 00:11:11 +0800

    first commit

---
+++ Index.php
@@ -0,0 +1,5 @@
+<?php
+require "vendor/autoload.php";
+
+$model = new \Acme\Test();
+$model->test();
---
+++ src/Test.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace Acme;
+
+
+class Test
+{
+    public function test() {
+        echo  "Form Test -> test";
+    }
+}
